90eb929d5f562a740b54a6e5944c4911c9d6dd9c
G.F <fanguangjianauto@163.com>
2024-07-30 23:10:52 +1000

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("Helo C#");
+// Console.WriteLine(args[0]);
+
+
+namespace HelloWorld
+{
+    internal static class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+    
+}
