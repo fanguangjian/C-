d5addb962e8e406133a490809fbce6770b87406c
G.F <fanguangjianauto@163.com>
2024-08-07 23:50:06 +1000

    loop

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90eb929d5f562a740b54a6e5944c4911c9d6dd9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2107fd4193f31a4c50e0be879bf3e5d2b8a989")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
