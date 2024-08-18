abc251601d3a15910f9a7d69fb4d59c0fe87e471
G.F <fanguangjianauto@163.com>
2024-08-18 22:30:39 +1000

    DB Connection

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
 // Console.WriteLine("Helo C#");
 // Console.WriteLine(args[0]);
 
+using System.Data;
+using Dapper;
 using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
 
 
 namespace HelloWorld
@@ -14,22 +17,29 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Computer myComputer = new Computer(){
-                Motherboard = "Z333",
-                HasWifi = true,
-                HasLTE = false,
-                ReleasesDate = DateTime.Now,
-                Price = 888.89m,
-                VideoCard = "RRR 556"
+            // Computer myComputer = new Computer(){
+            //     Motherboard = "Z333",
+            //     HasWifi = true,
+            //     HasLTE = false,
+            //     ReleasesDate = DateTime.Now,
+            //     Price = 888.89m,
+            //     VideoCard = "RRR 556"
 
-            };
-            Console.WriteLine(myComputer.Price);
+            // };
+            // Console.WriteLine(myComputer.Price);
             // Console.WriteLine("AAA:");
 
             // string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;"
-            // string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;User Id= SA;Password=SQLfantuan1";
+            string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;User Id= SA;Password=SQLfantuan1";
             
-            // IDbconnection dbConnection = new SqlConnection(connectionString);
+            IDbConnection dbConnection = new SqlConnection(connectionString);
+
+            string sqlCommand = "SELECT GETDATE()";
+
+            DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+            Console.WriteLine(rightNow);
+
 
 
 
