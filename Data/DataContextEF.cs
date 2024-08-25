823f207495a3f100235f98a1784a8d0716345fc4
G.F <fanguangjianauto@163.com>
2024-08-25 22:39:04 +1000

    Add EntityFramework

--- Data/DataContextEF.cs
+++ Data/DataContextEF.cs
@@ -1,7 +1,34 @@
+using System.Reflection.Emit;
+using HelloWorld.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace HelloWorld.Data
 {
-    public class DataContextEF
+    public class DataContextEF : DbContext
     {
+        
+        public DbSet<Computer> ? Computer { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // base.OnConfiguring(options);
+            if(!options.IsConfigured){
+                options.UseSqlServer(
+                    "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;User Id= SA;Password=SQLfantuan1",
+                    options => options.EnableRetryOnFailure()
+                );
+            }
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema");
+            // base.OnModelCreating(modelBuilder);
+            // modelBuilder.Entity<Computer>().HasNoKey();
+            modelBuilder.Entity<Computer>().HasKey( c => c.ComputerId);
+
+                // .ToTable("TableName","SchemaName");
+                // .ToTable("Computer","TutorialAppSchema");
+// 
+        }
     }
 }
--- Models/Computer.cs
+++ Models/Computer.cs
@@ -2,8 +2,9 @@ namespace HelloWorld.Models{
 
     public class Computer{
             // private string _motherboard;
+            public int ComputerId { get; set; }
             public string Motherboard {get; set; }
-            public int CPUCores {get; set; }
+            public int? CPUCores {get; set; }
             public bool HasWifi {get; set; }
             public bool HasLTE {get; set; }
             public DateTime ReleaseDate {get; set; }
@@ -16,6 +17,9 @@ namespace HelloWorld.Models{
                 }
                 if(this.Motherboard == null){
                     Motherboard = "";
+                }
+                if(this.CPUCores == null){
+                    CPUCores = 0;
                 }
             }
 
