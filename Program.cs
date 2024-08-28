dd88d1f53364ff6462abf93571a9be9234e5741c
G.F <fanguangjianauto@163.com>
2024-08-28 23:42:29 +1000

    CamelCase  Serialize

--- Program.cs
+++ Program.cs
@@ -5,12 +5,16 @@
 // Console.WriteLine(args[0]);
 
 using System.Data;
+using System.Text.Json;
 using Dapper;
 using HelloWorld.Data;
 using HelloWorld.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage.Json;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 
 namespace HelloWorld
@@ -20,97 +24,42 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            Computer myComputer = new Computer(){
-                Motherboard = "Z334",
-                HasWifi = true,
-                HasLTE = false,
-                ReleaseDate = DateTime.Now,
-                Price = 888.89m,
-                VideoCard = "RRR 557711"
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            DataContextDapper dapper = new DataContextDapper(config);
+
+            string computerJson = File.ReadAllText("Computers.json");
+
+            // Console.WriteLine(computerJson);
+            JsonSerializerOptions options = new JsonSerializerOptions(){
+                // PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseUpper
 
             };
-            // Console.WriteLine(myComputer.Price);
-            // Console.WriteLine("AAA:");
+            // class System.Text.Json.JsonSerializer
+            // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computerJson, options);
+            // package Newtonsoft Json  
+            IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computerJson);
+   
 
-            // string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;"
-            // string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;User Id= SA;Password=SQLfantuan1";
-            
-            // IDbConnection dbConnection = new SqlConnection(connectionString);
-
-            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-
-            // DataContextDapper dapper = new DataContextDapper();
-            // DataContextEF entityFramework = new DataContextEF();
-
-            DataContextDapper dapper = new DataContextDapper(config);
-            DataContextEF entityFramework = new DataContextEF(config);
-
-            string sqlCommand = "SELECT GETDATE()";
-
-            // DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
-            // DateTime rightNow = dapper.LoadDataSingle<DateTime>(sqlCommand);
-
-
-            // Console.WriteLine(rightNow);
-            // entityFramework.Add(myComputer);  // Instead of the following SQL
-            // entityFramework.SaveChanges();
-
-            string sql = @"INSERT INTO TutorialAppSchema.Computer (
-                Motherboard,
-                HasWifi,
-                HasLTE,
-                ReleaseDate,
-                Price,
-                VideoCard
-            ) VALUES (
-                  '" + myComputer.Motherboard 
-                  + "','" + myComputer.HasWifi 
-                  + "','" + myComputer.HasLTE
-                  + "','" + myComputer.ReleaseDate
-                  + "','" + myComputer.Price
-                  + "','" + myComputer.VideoCard
-
-            +"')";
-
-            // int res = dbConnection.Execute(sql);
-            // int res = dapper.ExecuteSqlWithRowCount(sql);
-            // bool res = dapper.ExecuteSql(sql);
-
-
-            // Console.WriteLine(res);
-
-            string sqlSelect = @"SELECT TOP(100)* FROM TutorialAppSchema.Computer";
-            // List<Computer> computers = dbConnection.Query<Computer>(sqlSelect).ToList();
-            // IEnumerable<Computer> computers = dbConnection.Query<Computer>(sqlSelect).ToList();
-            IEnumerable<Computer> computers = dapper.LoadData<Computer>(sqlSelect).ToList();
-            IEnumerable<Computer>? computersEf = entityFramework.Computer?.ToList<Computer>();
-
-
-
-            // foreach (var com in computers)
-            foreach (var com in computersEf)
-
-            {
-                // Console.WriteLine(com.Motherboard);
-                Console.WriteLine(
-                  "'" + com.Motherboard + "','" + com.HasWifi + "'"
-                );
-
+            if(computers != null){
+                foreach (Computer com in computers)
+                {
+                    // Console.WriteLine(com.Motherboard);
+                }
             }
 
-            // Console.WriteLine(computers);
-            // File.WriteAllText("log.txt",sql);
+            JsonSerializerSettings settings = new JsonSerializerSettings(){
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+      
+
+            };
+
+            string computerCopNewtonsoft = JsonConvert.SerializeObject(computers, settings);
+            File.WriteAllText("computerCopNewtonsoft.txt", computerCopNewtonsoft);
+
             
-            //  will write with no break
-            // using StreamWriter openFile = new("log.txt", append: true);
-            // openFile.WriteLine("\n" + sql + "\n" );
-            // openFile.Close();
-
-            string fileText = File.ReadAllText("log.txt");
-
-            Console.WriteLine(fileText);
-
-
+            string computerCopySystem = System.Text.Json.JsonSerializer.Serialize(computers, options);
+            File.WriteAllText("computerCopySystem.txt", computerCopySystem);
         }
     }
     
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: G.F
  * @Date: 2024-07-30 23:10:19
- * @LastEditTime: 2024-08-26 21:58:40
+ * @LastEditTime: 2024-08-28 23:35:15
  * @LastEditors: your name
  * @Description: 
  * @FilePath: /HelloWorld/README.md
@@ -17,6 +17,7 @@ dotnet add package microsoft.entityframeworkcore.sqlserver
 
 dotnet add package microsoft.extensions.configuration.JSON
 dotnet restore
+dotnet add package Newtonsoft.Json
 
 
 * Entity Framework Vss Dapper
