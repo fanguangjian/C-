fac0abfbb4a4125e8d81dcd091c2f5b8ecc3e6b6
G.F <fanguangjianauto@163.com>
2024-09-10 23:25:05 +1000

    Task.Delay() & Thread.Sleep()

--- Program.cs
+++ Program.cs
@@ -17,70 +17,48 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
+// Thread.Sleep: 同步方法，阻塞线程，适用于简单的线程休眠。
+// Task.Delay: 异步方法，不阻塞线程，适用于异步编程场景。
+// Thread.Sleep: synchronous method, blocking the thread, suitable for simple thread sleeping.
+// Task.Delay: asynchronous method, does not block the thread, suitable for asynchronous programming scenarios.
+
 
 namespace HelloWorld
 {  
    
     internal static class Program {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+           Task firstTask = new Task( () => {
+                Thread.Sleep(100);
+                Console.WriteLine("Task 1 100");
+           });
+           firstTask.Start();
+           Task secondTask = ConsoleAfterDelayAsync("Task 2 150", 150);
+           Task thirdTask = ConsoleAfterDelayAsync("Task 3 1500", 1500);
 
-            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            DataContextDapper dapper = new DataContextDapper(config);
+           ConsoleAfterDelay("Delay 1000", 1000);           
+           await firstTask;
+           Console.WriteLine("After the task was created");
+           ConsoleAfterDelay("Delay 2000", 2000);
+           await secondTask;
+           await thirdTask;
+           Console.WriteLine("After the task was created 123");
 
-            string computerJson = File.ReadAllText("ComputersSnake.json");
-
-            Mapper mapper = new Mapper(new MapperConfiguration((cfg) => {
-                cfg.CreateMap<ComputerSnake, Computer>()
-                    .ForMember(destination => destination.ComputerId, options => options.MapFrom(source => source.computer_id))
-                    .ForMember(destination => destination.Motherboard, options => options.MapFrom(source => source.motherboard))
-                    .ForMember(destination => destination.CPUCores, options => options.MapFrom(source => source.cpu_cores))
-                    .ForMember(destination => destination.HasWifi, options => options.MapFrom(source => source.has_wifi))
-                    .ForMember(destination => destination.HasLTE, options => options.MapFrom(source => source.has_lte))
-                    .ForMember(destination => destination.VideoCard, options => options.MapFrom(source => source.video_card))
-                    .ForMember(destination => destination.ReleaseDate, options => options.MapFrom(source => source.release_date))
-                    .ForMember(destination => destination.Price, options => options.MapFrom(source => source.price * .8m));
-
-            }));
-
-            // Console.WriteLine(computerJson);
-            JsonSerializerOptions options = new JsonSerializerOptions(){
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                // PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseUpper
-
-            };
-            // class System.Text.Json.JsonSerializer
-            // IEnumerable<Computer>? computers = JsonSerializer.Deserialize<IEnumerable<Computer>>(computerJson, options);
-            // package Newtonsoft Json  
-            // IEnumerable<Computer>? computers = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computerJson);
-            // IEnumerable<Computer>? computersNewtonSoft = JsonConvert.DeserializeObject<IEnumerable<Computer>>(computerJson);
-
-            // IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computerJson , options);
-            IEnumerable<ComputerSnake>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<ComputerSnake>>(computerJson);
-
-   
-
-            if(computersSystem != null){
-                IEnumerable<Computer> computerResult = mapper.Map<IEnumerable<Computer>>(computersSystem);
-                Console.WriteLine("Automapper Count: " +  computerResult.Count());
-                // foreach (Computer computer in computerResult)
-                // {
-                //     Console.WriteLine(computer.Motherboard);
-                // } 
-
-            }
-
-            JsonSerializerSettings settings = new JsonSerializerSettings(){
-                ContractResolver = new CamelCasePropertyNamesContractResolver()
-      
-
-            };        
         }
 
-        static string EscapeSingleQuote(string input){
-            string output = input.Replace("'", "''");
-            return output;
+        static void ConsoleAfterDelay( string text,  int delayTime){
+            Thread.Sleep(delayTime);
+            Console.WriteLine(text);
         }
+
+        static async Task ConsoleAfterDelayAsync( string text,  int delayTime){
+            // Thread.Sleep(delayTime);
+            await Task.Delay(delayTime);
+            Console.WriteLine(text);
+        }
+
+       
     }
     
 }
